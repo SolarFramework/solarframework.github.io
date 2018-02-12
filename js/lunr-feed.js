799986f75e0e4503adbfdeec17debd183e33930b
solarBcom <framework.solar@b-com.com>
2018-02-12 16:14:14 +0000

    Update solar site

--- js/lunr-feed.js
+++ js/lunr-feed.js
@@ -16,7 +16,7 @@ index.add({
 
 index.add({
     title: null,
-    content: "\n\n  \n    SolAR\n    SolAR\n    https://solarframework.github.io//\n    \n    Mon, 12 Feb 2018 16:10:22 +0000\n    Mon, 12 Feb 2018 16:10:22 +0000\n    Jekyll v3.5.1\n    \n  \n\n",
+    content: "\n\n  \n    SolAR\n    SolAR\n    https://solarframework.github.io//\n    \n    Mon, 12 Feb 2018 16:14:05 +0000\n    Mon, 12 Feb 2018 16:14:05 +0000\n    Jekyll v3.5.1\n    \n  \n\n",
     id: 1
   });
   
