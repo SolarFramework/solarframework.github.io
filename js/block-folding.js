4db98f3100e5ebbd4f22851ab76ee8b6fd2b1eec
solarBcom <framework.solar@b-com.com>
2018-02-12 16:03:25 +0000

    Update solar site

--- js/block-folding.js
+++ js/block-folding.js
@@ -1,18 +1,16 @@
 $(function() {
 
   // Init show and hide title bars to display the matching keyword
-  $(".blockShow .title").append("<span>[hide]</span>");
-  $(".blockHide .title").append("<span>[show]</span>");
+  $(".blockShow .title").append('<span class="display-action">[hide]</span>');
+  $(".blockHide .title").append('<span class="display-action">[show]</span>');
 
   // Listen to click on folding title bars and do the matching action
-  $(".blockShow, .blockHide").click(function(event) {
-    //event.preventDefault();
+  $(".blockShow .display-action, .blockHide .display-action").click(function(event) {
     // Update title [action] label
-    let titleAction = $(this).find(".title span").html();
-    $(this).find(".title span").html( titleAction === "[show]" ? "[hide]" : "[show]");
-    // Invert classes (show becomes hide and vice versa) to get css display none/block working
-    $(this).toggleClass("blockShow");
-    $(this).toggleClass("blockHide");
+    const $this = $(this);
+    const titleAction = $this.html();
+    $this.html(titleAction === "[show]" ? "[hide]" : "[show]");
+    // Invert classes (show becomes hide and vice versa) to get css display none / block working
+    $this.closest('.blockShow, .blockHide').toggleClass('blockShow blockHide');
   });
-
 });
