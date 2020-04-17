50ab1fb097524c50fc37773d4dd7e91c0826b150
solarBcom <framework.solar@b-com.com>
2020-04-17 13:44:08 +0000

    Update solar site

--- js/lunr-feed.js
+++ js/lunr-feed.js
@@ -65,7 +65,7 @@ index.add({
 
 index.add({
     title: null,
-    content: "\n\n  \n    SolAR\n    SolAR\n    https://solarframework.github.io//\n    \n    Wed, 15 Apr 2020 00:41:01 +0000\n    Wed, 15 Apr 2020 00:41:01 +0000\n    Jekyll v3.5.1\n    \n  \n\n",
+    content: "\n\n  \n    SolAR\n    SolAR\n    https://solarframework.github.io//\n    \n    Fri, 17 Apr 2020 13:43:54 +0000\n    Fri, 17 Apr 2020 13:43:54 +0000\n    Jekyll v3.5.1\n    \n  \n\n",
     id: 8
   });
   
