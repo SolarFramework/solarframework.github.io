9b0d21b07a1818ff1db0ab1eb8f452ca672b4de9
solarBcom <framework.solar@b-com.com>
2021-01-07 15:59:28 +0000

    Update SolAR Module APIs

--- api_module_documentation/search/all_2.js
+++ api_module_documentation/search/all_2.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['calibrate',['calibrate',['../class_sol_a_r_1_1_m_o_d_u_l_e_s_1_1_o_p_e_n_c_v_1_1_sol_a_r_camera_calibration_opencv.html#a985438e0e96759ad99d013cda5a53c48',1,'SolAR::MODULES::OPENCV::SolARCameraCalibrationOpencv::calibrate(const std::string &amp;inputVideo, const std::string &amp;output) override'],['../class_sol_a_r_1_1_m_o_d_u_l_e_s_1_1_o_p_e_n_c_v_1_1_sol_a_r_camera_calibration_opencv.html#a967fc21b9341a58b440e3dc6562ae12d',1,'SolAR::MODULES::OPENCV::SolARCameraCalibrationOpencv::calibrate(int camera_id, const std::string &amp;output) override']]],
   ['clear',['clear',['../class_sol_a_r_1_1_m_o_d_u_l_e_s_1_1_t_o_o_l_s_1_1_sol_a_r_boost_covisibility_graph.html#ac784e82d8eaf18169cd35343d618181a',1,'SolAR::MODULES::TOOLS::SolARBoostCovisibilityGraph']]],
-  ['correct',['correct',['../class_sol_a_r_1_1_m_o_d_u_l_e_s_1_1_t_o_o_l_s_1_1_sol_a_r_loop_corrector.html#abea6dde71c1fda79ee0b4ca81ef496df',1,'SolAR::MODULES::TOOLS::SolARLoopCorrector']]]
+  ['correct',['correct',['../class_sol_a_r_1_1_m_o_d_u_l_e_s_1_1_t_o_o_l_s_1_1_sol_a_r_loop_corrector.html#a86e1df9416d7563ffc01014d96aed12f',1,'SolAR::MODULES::TOOLS::SolARLoopCorrector']]]
 ];
--- api_module_documentation/search/functions_1.js
+++ api_module_documentation/search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['bundleadjustment',['bundleAdjustment',['../class_sol_a_r_1_1_m_o_d_u_l_e_s_1_1_c_e_r_e_s_1_1_sol_a_r_bundler_ceres.html#a178e5963575881f96fde5bba883fadf8',1,'SolAR::MODULES::CERES::SolARBundlerCeres::bundleAdjustment()'],['../class_sol_a_r_1_1_m_o_d_u_l_e_s_1_1_g2_o_1_1_sol_a_r_optimization_g2_o.html#abba5ecdcf719970f59edd55450a763cc',1,'SolAR::MODULES::G2O::SolAROptimizationG2O::bundleAdjustment()']]]
+  ['bundleadjustment',['bundleAdjustment',['../class_sol_a_r_1_1_m_o_d_u_l_e_s_1_1_c_e_r_e_s_1_1_sol_a_r_bundler_ceres.html#aca1f078bb4ffd7ffcbd70e3e4ee0b1eb',1,'SolAR::MODULES::CERES::SolARBundlerCeres::bundleAdjustment()'],['../class_sol_a_r_1_1_m_o_d_u_l_e_s_1_1_g2_o_1_1_sol_a_r_optimization_g2_o.html#aa9925b365ec7b1f66ec02513e846f270',1,'SolAR::MODULES::G2O::SolAROptimizationG2O::bundleAdjustment()']]]
 ];
